a3330a74a74fea5a791de6a78e93571cfccf8011
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-02-04 03:38:15 -0800

    add rosetta urls and i18n_patterns to urls.py

--- django_lang/urls.py
+++ django_lang/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('course.urls')),
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('course.urls')),
+#     path('rosetta/', include('rosetta.urls')),
+# ]
+
+from django.utils.translation import gettext_lazy as _
+
+urlpatterns = i18n_patterns(
+    path(_('admin/'), admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
-]
+    path('', include('course.urls')),
+)
